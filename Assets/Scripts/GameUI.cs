e0b2ce1db2039b89e4d26e1582ab9b453647824f
Justin <justin.c515@gmail.com>
2019-07-05 20:59:31 -0400

    UI Scripts

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+
+  [Header("HUD")]
+  public TextMeshProUGUI scoreText;
+  public TextMeshProUGUI ammoText;
+  public Image healthBarFill;
+
+  [Header("Pause Menu")]
+  public GameObject pauseMenu;
+
+  [Header("Endgame Screen")]
+  public GameObject endGameScreen;
+  public TextMeshProUGUI endGameHeaderText;
+  public TextMeshProUGUI endGameScoreText;
+
+  //instance
+
+  public static GameUI instance;
+
+  private void Awake()
+  {
+
+    instance = this;
+
+  }
+
+  public void UpdateHealthBar(int curHp, int maxHp)
+  {
+
+    healthBarFill.fillAmount = (float)curHp / (float)maxHp;
+
+  }
+
+  public void UpdateScoreText(int score)
+  {
+
+    scoreText.text = "Score: " + score;
+
+  }
+
+  public void UpdateAmmoText(int curAmmo, int maxAmmo)
+  {
+
+    ammoText.text = "Ammo: " + curAmmo + " / " + maxAmmo;
+
+  }
+
+  public void TogglePauseMenu(bool paused)
+  {
+
+    pauseMenu.SetActive(paused);
+  
+  }
+
+  public void SetEndGameScreen(bool won, int score)
+  {
+
+    endGameScreen.SetActive(true);
+    endGameHeaderText.text = won == true ? "You Win" : "You Lose";
+    endGameHeaderText.color = won == true ? Color.green : Color.red;
+    endGameScoreText.text = "<b>Score</b>\n" + score;
+
+  }
+
+  public void OnResumeButton()
+  {
+
+    
+
+  }
+
+  public void OnRestartButton()
+  {
+
+    SceneManager.LoadScene("Game");
+
+  }
+
+  public void OnMenuButton()
+  {
+
+    SceneManager.LoadScene("Menu");
+
+  }
+
+}
